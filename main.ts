bdfee0eb60930174fc07fb922db9394972edb72d
vlosz <156816464+vlosz@users.noreply.github.com>
2024-01-22 09:39:35 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,12 @@
 basic.forever(function () {
-	
+    if (input.temperature() > 20) {
+        pins.digitalWritePin(DigitalPin.P0, 1)
+    } else {
+        pins.digitalWritePin(DigitalPin.P0, 0)
+    }
+    if (pins.digitalReadPin(DigitalPin.P1) == 1) {
+        motors.dualMotorPower(Motor.M0, 100)
+    } else {
+        motors.motorCommand(MotorCommand.Break)
+    }
 })
